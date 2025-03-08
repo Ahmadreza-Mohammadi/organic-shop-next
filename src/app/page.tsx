252e91f3918d65a4bb5711bcc0ea56336c149dd2
noway777 <ahmadrezamohammadi777@gmail.com>
2025-03-08 14:53:31 +0330

    feat: products page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Products from "@/pages/products";
+
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
-    <p className="text-blue-600">Lorem ipsum dolor sit amet.</p>
+      <Products />
     </>
   );
 }
---
+++ src/components/products/products.tsx
@@ -0,0 +1,24 @@
+function ProductsComponent() {
+  const sortFilters = [
+    { name: "جدیدترین", id: 1 },
+    { name: "قدیمی ترین", id: 2 },
+    { name: "ارزان ترین", id: 3 },
+    { name: "گران ترین", id: 4 },
+  ];
+  return (
+    <div className="flex justify-center">
+      <div className="bg-red-400 mt-10 w-[1280px] p-4 flex gap-3">
+        <label>مرتب شده:</label>
+        <select className="p-1 w-[130px]" name="" id="">
+          {sortFilters.map((filter) => (
+            <option key={filter.id} value="">
+              {filter.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsComponent;
