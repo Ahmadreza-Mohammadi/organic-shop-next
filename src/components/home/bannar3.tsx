94dc87fb8daed47712f7c98c1ba5af6739d09a11
fateme sofyan <fatemesofyan1576@gmail.com>
2025-03-10 22:17:51 +0330

    feat:bannar3

--- src/components/home/bannar3.tsx
+++ src/components/home/bannar3.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+
+import discountBannar from "@/assets/images/baner/Discount Bannar.png";
 
 export default function Bannar3() {
   return (
-    <div>
-        <p>فروش تابستانی 37% تخفیف رایگان برای کلیه سفارشات شما، ارسال رایگان و 30 روز ضمانت بازگشت وجه</p>
+    <div className="relative w-full flex justify-center items-center  ">
+         <Image
+                      src={discountBannar}
+                      alt="bannarBag"
+                      width={423}
+                      height={288}
+                      className="rounded-lg object-cover w-[1300px] "
+                    />
+                      
+          <p className="absolute right-44 top-1/4  flex flex-col  gap-5 items-center justify-center text-white text-xl font-bold ">
+            فروش تا{" "}
+            <span className="bg-orange-500 p-2 rounded-lg">30%تخفیف</span>
+            <span className="text-sm">ارسال رایگان برای تمام سفارشات شما</span>
+          </p>
     </div>
   )
 }
