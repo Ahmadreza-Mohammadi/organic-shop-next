3fc85f6a8f9520a3ae70dfc9af753fbe72f422c8
noway777 <ahmadrezamohammadi777@gmail.com>
2025-03-10 15:01:22 +0330

    fix: styles in navbar fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,19 @@
+"use client"
+
 import AboutCo from "@/components/about/aboutCo/aboutCo";
 import CustomerReviews from "@/components/about/customerReviews/customerReviews";
 import Team from "@/components/about/team/team";
+import Footer from "@/components/footer/footer";
+import Navbar from "@/components/navbar/navbar";
 
 function about() {
   return (
-    <>
+    <div>
       <Navbar />
       <AboutCo />
       <Team />
       <CustomerReviews />
+      <Footer />
     </div>
   );
 }
--- src/app/single_product/page.tsx
+++ src/app/single_product/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+
+import Footer from "@/components/footer/footer";
+import Navbar from "@/components/navbar/navbar";
 import TopSingle from "@/components/single-product/single-first";
 import MidleSingle from "@/components/single-product/single-second";
-import React, { useState } from "react";
 function SingleProduct() {
   return (
     <main className="bg-white flex flex-col justify-center">
+      <Navbar />
       <TopSingle />
       <MidleSingle />
+      <Footer />
     </main>
   );
 };
