0cfde3221bc2bbe0feceefb7677aad9ca839b980
fateme sofyan <fatemesofyan1576@gmail.com>
2025-03-10 20:27:26 +0330

    feat:bannar2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,6 +5,8 @@ import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 import React from "react";
 import Products from "@/components/home/products";
+import Bannar2 from "@/components/home/bannar2";
+import Bannar3 from "@/components/home/bannar3";
 
 function Home() {
   return (
@@ -14,6 +16,8 @@ function Home() {
       <Feature />
       <Categories/>
       <Products/>
+      <Bannar2/>
+      <Bannar3/>
            <Footer />
     </>
   );
---
+++ src/components/home/bannar3.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bannar3() {
+  return (
+    <div>
+        <p>فروش تابستانی 37% تخفیف رایگان برای کلیه سفارشات شما، ارسال رایگان و 30 روز ضمانت بازگشت وجه</p>
+    </div>
+  )
+}
